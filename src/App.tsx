38b7ead19cad0b366e1f6bfbff83470320515310
José Vitor da Silva Santos <jvss1036@gmail.com>
2022-08-26 15:54:43 -0300

    feature: Component Header Made

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Header } from './Components/Header';
 
 function App() {
   return (
     <>
-      
+      <Header/>
     </>
   );
 }
---
+++ src/Components/Menu/index.tsx
@@ -0,0 +1,66 @@
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+
+export const MenuOptions = () => {
+  return (
+    <Menu>
+      <MenuButton as="button" aria-label="Options">
+        <HamburgerIcon
+          fontSize="35px"
+          color="black"
+          _dark={{ color: "#7f7f7f" }}
+        />
+      </MenuButton>
+      <MenuList
+        color="#212529"
+        _dark={{color: "#7f7f7f", background: "#0c0c0d"}}
+        
+      >
+        <MenuItem
+        fontWeight="bold"
+        fontSize="18px"
+        _hover={{
+          color: "#1747A6",
+          transition: "ease 0.6s",
+          opacity: "0.8",
+        }}
+        >
+          Sobre
+        </MenuItem>
+        <MenuItem
+        fontWeight="bold"
+        fontSize="18px"
+        _hover={{
+          color: "#1747A6",
+          transition: "ease 0.6s",
+          opacity: "0.8",
+        }}
+        >
+          Projetos
+        </MenuItem>
+        <MenuItem
+        fontWeight="bold"
+        fontSize="18px"
+        _hover={{
+          color: "#1747A6",
+          transition: "ease 0.6s",
+          opacity: "0.8",
+        }}
+        >
+          Resumo
+        </MenuItem>
+        <MenuItem
+        fontWeight="bold"
+        fontSize="18px"
+        _hover={{
+          color: "#1747A6",
+          transition: "ease 0.6s",
+          opacity: "0.8",
+        }}
+        >
+          Contato
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
